5df52ee3705b6bc93a565737eaf6c9c7eef86848
Ahmed <helmyahmed248@gmail.com>
2022-04-23 23:20:12 +0200

    first commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,6 +11,6 @@ class Category extends Model
     protected $guarded = [];
     public function products()
     {
-     $this->hasMany(Product::class,'category_id','id');
+     return $this->hasMany(Product::class,'category_id','id');
     }
 }
