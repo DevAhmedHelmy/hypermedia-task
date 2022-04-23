2fc4032de9f0177bc31ea72ab271fd2b78469157
Ahmed <helmyahmed248@gmail.com>
2022-04-23 15:16:12 +0200

    first commit

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -14,6 +14,10 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'created_at' =>$this->created_at->format('M d Y')
+        ];
     }
 }
--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -14,6 +14,12 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return  [
+            'id' => $this->id,
+            'name' => $this->name,
+            'category_id'=>$this->category_id,
+            'category' => $this->category->name,
+            'created_at' =>$this->created_at->format('M d Y')
+        ];
     }
 }
